b6c5a84e484f88e84edc0a054bab10726922c89b
niksata-stareca <nikola.iv.663@gmail.com>
2025-06-17 23:28:32 +0300

    Create random_sentence_generator.py
    uploaded the base version of the program

---
+++ random_sentence_generator.py
@@ -0,0 +1,24 @@
+import random
+
+def get_random_words(words):
+    return random.choice(words)
+
+names = ['Ivan', 'Nikola', 'Nikolay', 'Viktor', 'Mihal']
+places = ['Varna', 'Sofia', 'Plovdiv', 'Chirpan', 'Burgas']
+verbs = ['eats', 'smokes', 'watches', 'rides', 'hides']
+nouns = ['cigarette', 'hamburger', 'cake', 'camppfire', 'bikes']
+adverbs = ['slowly', 'carefully', 'fast', 'uncontrollably', 'miserably']
+details = ['at school', 'after school', 'at home', 'near the sea', 'while being watched']
+
+
+print('Hello, this is your first random sentence: \n')
+while True:
+    random_name = get_random_words(names)
+    random_place = get_random_words(places)
+    random_verb = get_random_words(verbs)
+    random_noun = get_random_words(nouns)
+    random_adverb = get_random_words(adverbs)
+    random_detail = get_random_words(details)
+
+    print(f'{random_name} from {random_place} {random_adverb} {random_verb} {random_noun}.')
+    input('Press [Enter] to generate a new sentence. \n')
